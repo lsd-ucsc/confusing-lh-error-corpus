a35f86ef7d6584ecc4e43a3e4777981a050cc596
PLR <51248199+plredmond@users.noreply.github.com>
2021-09-30 11:10:07 -0700

    Autocommit

--- lib/Hw1.hs
+++ lib/Hw1.hs
@@ -51,6 +51,7 @@ sub' x y
 
 {-@ LIQUID "--exact-data" @-}
 {-@ LIQUID "--exact-data-cons" @-}
+{-@ LIQUID "--no-adt" @-}
 {-@ halve :: i:Int -> (Int, Int)<{\j1 j2 -> j1 == div i 2 && j2 == j1 + mod i 2}> @-}
 halve :: Int -> (Int, Int)
 halve i = (j, j + r)
